5e15ba312e803139207825fc2e4409f9a489e0ca
Luis Angel <neal.ocaffry@gmail.com>
2024-10-11 17:10:13 -0600

    CAMBIOS, HAY QUE AGREGAR LA FUNCIONALIDAD DE EMPLEADOS

--- app/Controller/EmpleadosController.py
+++ app/Controller/EmpleadosController.py
@@ -4,6 +4,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from ..View.UserInterfacePy.UI_CONTROL_EMPLEADOS import *
 from .RolesController import *
+from .RegistroInicialController import *
 
 
 class Empleados(QWidget):
@@ -11,10 +12,14 @@ class Empleados(QWidget):
         super().__init__()
         self.ui = Ui_Control_empleados()
         self.ui.setupUi(self)
-        # self.ui.btn_btn_agregar.clicked.connect(self.agregar_nuevo_empleado)
+        self.ui.btn_btn_agregar.clicked.connect(self.agregar_empleado)
         self.ui.btn_btn_controlroles.clicked.connect(self.roles)
     
     def roles(self):
         self.roles =  Control_rol()
         self.roles.show()
+
+    def agregar_empleado(self):
+        self.ventana = Registro_personal_inicial()
+        self.ventana.show()
 
