8cec16aa5c3dba3ad5c31dedc5de44f2b8a9a39d
Luis Angel <neal.ocaffry@gmail.com>
2024-12-04 12:55:02 -0600

    CAMBIOS EN LAS VENTANAS

--- app/View/UserInterfaceUi/UI_CREAR_ORDEN_DE_COMPRA_ui.py
+++ app/View/UserInterfaceUi/UI_CREAR_ORDEN_DE_COMPRA_ui.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'UI_CREAR_ORDEN_DE_COMPRA.ui'
+# Form implementation generated from reading ui file 'c:\Users\Angel Rodriguez\LUIS_ANGEL_VALLEJO\software-punto-de-venta\app\View\UserInterfaceUi\UI_CREAR_ORDEN_DE_COMPRA.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.11
 #
